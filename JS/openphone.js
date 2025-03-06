9a9e9750e5bca50074699ac37582a389b5911d22
Trash King <reedtakesemail@gmail.com>
2025-03-05 22:12:38 -0800

    Add phone number on about me and radio page, change wording of radio about section

---
+++ JS/openphone.js
@@ -0,0 +1,13 @@
+// Get references to the elements
+const modal = document.getElementById('modal');
+const thumbnail = document.getElementById('thumbnail');
+
+// Open the modal when the thumbnail is clicked
+thumbnail.addEventListener('click', () => {
+modal.style.display = 'flex';
+});
+
+// Close the modal when clicking anywhere on it
+modal.addEventListener('click', () => {
+modal.style.display = 'none';
+});
